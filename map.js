94755b1ec33ff115505e9ee72dab5c4c3595e9ae
Devon Matsalla <dpmatsalla@hotmail.com>
2023-08-05 22:38:31 +1000

    Update map.js

--- map.js
+++ map.js
@@ -1,11 +1,12 @@
 // Initialize the map centered on Brisbane
-var map = L.map('map').setView([-27.468968, 153.000492], 14);
+var map = L.map('map').setView([ -27.488299, 152.996411], 14);
 
 // Add the Tile Layer (you can change the tile URL to other providers if needed)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
-// Add a marker to Orleigh Park
-var orleighParkMarker = L.marker([-27.477742, 153.004248]).addTo(map);
+// Add markers 
+var orleighParkMarker = L.marker([-27.488299, 152.996411]).addTo(map);
+var rockMarker = L.marker([-27.504172, 153.009583]).addTo(map);
 
 // Bind a popup to the marker
-orleighParkMarker.bindPopup("<b>Orleigh Park</b><br>Located here.").openPopup();
+orleighParkMarker.bindPopup("<b>Orleigh Park</b>").openPopup();
