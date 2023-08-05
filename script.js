3fe418c93235baa7d3bfbe66279b4dfc71984407
Devon Matsalla <dpmatsalla@hotmail.com>
2023-08-05 20:33:14 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 // Function to group the tide information by date and separate low and high tides
 function groupTidesByDate(tide_list) {
-    var tidesByDate = {}
+    var tidesByDate = [];
     tide_list.forEach(function(tide) {
         var dateStr = tide.time_local.split('T')[0];
         var timeStr = tide.time_local.split('T')[1].substr(0,5);
-        if (!tidesByDate[dateStr]) {
-            tidesByDate[dateStr] = { 'types': [], 'times': [], 'heights': [] };
-        }
-        tidesByDate[dateStr].types.push(tide.tide);
-        tidesByDate[dateStr].times.push(timeStr);
-        tidesByDate[dateStr].heights.push(tide.height);
+        //if (!tidesByDate[dateStr]) {
+        //    tidesByDate[dateStr] = { 'types': [], 'times': [], 'heights': [] };
+        //}
+        //tidesByDate[dateStr].types.push(tide.tide);
+        //tidesByDate[dateStr].times.push(timeStr);
+        //tidesByDate[dateStr].heights.push(tide.height);
+        tidesByDate.push({ date: dateStr, tide: timeStr+": "+tide.tide+" "+tide.height });
     });
     return tidesByDate;
 }
@@ -24,15 +25,14 @@ function tideHeight(t, tide_list) {
     var t1 = tide_list[i].time_stamp;
     var h0 = tide_list[i-1].height;
     var h1 = tide_list[i].height;
-    return (h0 - h1)/2 * Math.cos(Math.PI*(t0 - t)/(t0 - t1)) + (h0 + h1)/2;
+    return (h0-h1)/2*Math.cos(Math.PI*(t0-t)/(t0-t1)) + (h0+h1)/2;
 }
-
 
 // Generate calendar and events
 function generateCalendar() {
   const tableBody = document.querySelector("#calendar tbody");
   let currentDate = new Date();
-  currentDate.setDate(currentDate.getDate() - currentDate.getDay());  #set to the last Sunday
+  currentDate.setDate(currentDate.getDate() - currentDate.getDay());  //set to the last Sunday
 
   // Dummy list of events
   const events = [
@@ -73,6 +73,12 @@ function generateCalendar() {
 
     tableBody.appendChild(row);
   }
+}
+
+// Code starts here
+// add the timestamp to the tide_list
+for (var i=0;i<tide_list.length;i++) {
+    tide_list[i].time_stamp = Date.parse(tide_list[i].time_local);
 }
 
 // Call function to generate the calendar
