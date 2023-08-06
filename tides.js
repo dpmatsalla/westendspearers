f53351013dd2ee902676bbae61877699e7f77e76
Devon Matsalla <dpmatsalla@hotmail.com>
2023-08-06 16:03:49 +1000

    Update tides.js

--- tides.js
+++ tides.js
@@ -4,17 +4,18 @@ function drawCurve() {
     const amplitude = 50; // Change this value to adjust the height of the curve
     const frequency = 0.02; // Change this value to adjust the frequency of the curve
     const phaseShift = 0; // Change this value to adjust the phase shift of the curve
-    const yOffset = canvas.height / 2; // To center the curve vertically
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
 
     const currentDate = new Date();
-    const timestamp = currentDate.getTime();
+    const timeStart = currentDate.getTime();
+    const duration = 2 * 24*3600*1000;  //two days hence
+    const timeEnd = timeStart + duration;
     
-    for (let x = 0; x < canvas.width; x++) {
-      const y = amplitude * Math.sin(frequency * x + phaseShift) + yOffset;
-      ctx.lineTo(x, y);
+    for (let x = timeStart; x < timeEnd; x += 1*3600) {
+      const y = amplitude * tideHeight(x) + canvas.height/2;  // To center the curve vertically
+      ctx.lineTo((x - timeStart)*canvas.width/duration, y);
     }
 
     ctx.strokeStyle = '#000';
