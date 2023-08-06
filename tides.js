08f6aa7e23474dce6dbafd91e97ed409dabc534e
Devon Matsalla <dpmatsalla@hotmail.com>
2023-08-06 18:09:16 +0800

    Update tides.js

--- tides.js
+++ tides.js
@@ -1,26 +1,28 @@
 function drawCurve() {
     const canvas = document.getElementById('myCanvas');
     const ctx = canvas.getContext('2d');
-    const amplitude = 100; // Change this value to adjust the height of the curve
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.beginPath();
+    const amp = 100; // amplitude
+    const xx = canvas.width;
+    const yy = canvas.height;
 
     const currentDate = new Date();
     const timeStart = currentDate.getTime();
     const duration = 2 * 24*3600*1000;  //two days hence
     const timeEnd = timeStart + duration;
     
-    ctx.moveTo(0,canvas.height);
-    ctx.lineTo(canvas.width,canvas.height);
+    ctx.clearRect(0, 0, xx, yy);
+    ctx.beginPath();
+    ctx.rect(0, yy, xx, yy - 3*amp);
+    ctx.rect(0, yy - amp, xx, yy - 2*amp);
     ctx.strokeStyle = '#111';
     ctx.stroke();
 
+    
+    ctx.beginPath();
     for (let x = timeStart; x < timeEnd; x += 1*3600) {
-      const y = canvas.height - amplitude * tideHeight(x);
-      ctx.lineTo((x - timeStart)*canvas.width/duration, y);
+      const y = amp*tideHeight(x);
+      ctx.lineTo((x - timeStart)*xx/duration, yy - y);
     }
-
     ctx.strokeStyle = '#000';
     ctx.stroke();
 }
